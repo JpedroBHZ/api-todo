e62488c256338042b47acbab2fae2075dc009c12
JpedroBHZ <jpedro.bergamasco@gmail.com>
2026-06-09 09:50:34 -0300

    test(task): implementar testes unitários para TaskService
    - Cria estrutura de testes isolada usando JUnit 5 e Mockito
    - Valida o cenário de sucesso na criação de uma nova tarefa

---
+++ src/test/java/com/JpedroBHZ/todo/service/TaskServiceTest.java
@@ -0,0 +1,53 @@
+package com.JpedroBHZ.todo.service;
+
+import com.JpedroBHZ.todo.dto.TaskRequestDTO;
+import com.JpedroBHZ.todo.dto.TaskResponseDTO;
+import com.JpedroBHZ.todo.model.Task;
+import com.JpedroBHZ.todo.repository.TaskRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class TaskServiceTest {
+
+    @Mock
+    private TaskRepository repository;
+
+    @InjectMocks
+    private TaskService service;
+
+    @Test
+    @DisplayName("Deve salvar uma tarefa com sucesso quando os dados forem válidos")
+    void saveWithSuccess() {
+        // Arrange (Preparação dos dados)
+        TaskRequestDTO request = new TaskRequestDTO("Estudar JUnit e Mockito no Java");
+
+        Task savedTask = new Task();
+        savedTask.setId(1L);
+        savedTask.setDescription(request.description());
+        savedTask.setCompleted(false);
+
+        // Simulando o comportamento do Repository (Mock)
+        when(repository.save(any(Task.class))).thenReturn(savedTask);
+
+        // Act (Execução da ação que queremos testar)
+        TaskResponseDTO response = service.save(request);
+
+        // Assert (Verificações se tudo ocorreu como esperado)
+        assertNotNull(response);
+        assertEquals(1L, response.id());
+        assertEquals("Estudar JUnit e Mockito no Java", response.description());
+        assertFalse(response.completed());
+
+        // Garante que o repository foi chamado exatamente 1 vez
+        verify(repository, times(1)).save(any(Task.class));
+    }
+}
